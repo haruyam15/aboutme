114d328f7eb23f7432f181551a881b5063d5f33e
Haeun <haruyam15@gmail.com>
2023-03-27 16:37:54 +0900

    project 텍스트수정, 메타태그 삽입

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
 
 export default function Header(){
     const navs = ["Home", "Skills", "Project", "Study", "Contact"];
-    const [activeIndex, setActiveIndex] = useState(0)
 
     const handleNavScroll = (event) => {        
         const target = event.target.innerText;
@@ -10,22 +8,11 @@ export default function Header(){
         
         if(target === navs[0]){ //Home
             window.scrollTo({top:0,behavior:'smooth'});
-            setActiveIndex(0);
         }else { 
             const scrollTo = document.getElementById(target.toLowerCase()).offsetTop - headerHeight - 80 ;
             window.scrollTo({top: scrollTo, behavior: 'smooth'});
-            setActiveIndex(navs.indexOf(target));
         }
     }
-
-    // const handleScroll = () => {
-    //     const scrollPosition = window.scrollY;
-
-    // }
-
-    // useEffect(() => {    
-    //     window.addEventListener("scroll", handleScroll);
-    // }, []);
 
     return (
         <header id="nav" className="fixed w-full top-0 left-0 py-8 bg-white z-50">
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,17 +13,18 @@ export default function Project(){
                             <div>
                                 <p>PHP코드로 만들어져있던 오래된 사내 인트라넷 개편했습니다.</p>
                                 <p>오래전부터 여러 명을 거치면서 소스를 알아보기 힘들었고 디자인도 올드했습니다.</p>
-                                <p>작업은 부트스트랩 테마를 사용해 화면을 먼저 만들고, 필요한 기능들을 기존 소스에서 떼어와 입히는 방식으로 진행했습니다.</p>
+                                <p>코드를 작성할 때 주석을 적극 활용하여 누가 보아도 쉽게 알아볼 수 있도록 작성하려 노력했습니다.</p>
+                                <p>부트스트랩 테마를 활용해 화면을 먼저 만들고, 필요한 기능들을 기존 소스에서 떼어와 입히는 방식으로 진행했습니다.</p>
                                 <p>퍼블리셔 포지션으로만 근무하다가, 이 작업을 계기로 업무영역을 넓힐 수 있었습니다.</p>
                             </div>
 
                             <ul className="flex justify-start items-center mt-5">
-                                <li className="mr-2"><img src="./images/html5.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/css3.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/js.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/jquery.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/bootstrap.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/ajax.png" alt="" width="50" /></li>
+                                <li className="mr-2"><img src="./images/html5.png" alt="html" width="50" /></li>
+                                <li className="mr-2"><img src="./images/css3.png" alt="css3" width="50" /></li>
+                                <li className="mr-2"><img src="./images/js.png" alt="js" width="50" /></li>
+                                <li className="mr-2"><img src="./images/jquery.png" alt="jquery" width="50" /></li>
+                                <li className="mr-2"><img src="./images/bootstrap.png" alt="bootstrap" width="50" /></li>
+                                <li className="mr-2"><img src="./images/ajax.png" alt="ajax" width="50" /></li>
                             </ul>
                         </div>
                        <img src="./images/intranet.gif" width="600" alt="" />
@@ -52,19 +53,16 @@ export default function Project(){
                             </div>
 
                             <ul className="flex justify-start items-center mt-5">
-                                <li className="mr-2"><img src="./images/html5.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/css3.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/js.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/jquery.png" alt="" width="50" /></li>
-                                <li className="mr-2"><img src="./images/bootstrap.png" alt="" width="50" /></li>
+                                <li className="mr-2"><img src="./images/html5.png" alt="html5" width="50" /></li>
+                                <li className="mr-2"><img src="./images/css3.png" alt="css3" width="50" /></li>
+                                <li className="mr-2"><img src="./images/js.png" alt="js" width="50" /></li>
+                                <li className="mr-2"><img src="./images/jquery.png" alt="jquery" width="50" /></li>
+                                <li className="mr-2"><img src="./images/bootstrap.png" alt="bootstrap" width="50" /></li>
                             </ul>
                         </div>
 
                        <img src="./images/cindi.gif" width="600" alt="" />
                     </div>
-                </li>
-                <li>
-                    <p className="text-2xl font-bold text-left mb-10">• 이벤트 페이지</p>
                 </li>
             </ul>
         </div>
